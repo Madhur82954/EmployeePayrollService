2d29d986e566d3a95cf36822e350059db046e45d
Madhur82954 <itzmadhurverma@gmail.com>
2023-06-22 15:10:03 +0530

    [Madhur] - UC1-CreateDatabase Added

---
+++ Payroll.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayrollService
+{
+    internal class Payroll
+    {
+        public static void createdatabase()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(@"data source=DESKTOP-SAGJTNV\SQLEXPRESS;initial catalog=master;integrated security=true");
+                con.Open();
+                string query = "create database payroll_service;";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("Database created Succesfully");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmployeePayrollService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Payroll.createdatabase();
+        }
+    }
+}
